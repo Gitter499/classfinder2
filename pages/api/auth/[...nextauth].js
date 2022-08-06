50d2065affd9bf9f2b8c693dc6acd9edd8a8bcf6
Gitter499 <rafayel.amirkhanyan@gmail.com>
2022-08-05 22:18:12 -0400

    fixed whoopsy doopsie

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,17 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { db } from "../../../util/db"
+import GoogleProvider from "next-auth/providers/google";
 
 
 
 export default NextAuth({
     adapter: PrismaAdapter(db),
-    
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        })
+    ]
  
 })
